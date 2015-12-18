7e50116bb0519ced1d28340ecb19109c1169a580
Eric Carmichael <eric@ckcollab.com>
2015-12-18 02:10:56 -0800

    stderr tracked now with stdout. python helper auto inserts unbuffered flag

--- src/workers/tasks.py
+++ src/workers/tasks.py
@@ -1,3 +1,4 @@
+import itertools
 import signal
 import subprocess
 import sys
@@ -5,7 +6,7 @@ import time
 
 from celery import task
 from django.core.cache import cache
-from threading  import Thread
+from threading import Thread
 from Queue import Queue, Empty
 
 from submissions.models import Submission
@@ -16,10 +17,19 @@ from submissions.models import Submission
 ON_POSIX = 'posix' in sys.builtin_module_names
 
 
-def enqueue_output(out, queue):
-    for line in iter(out.readline, b''):
-        queue.put(line)
-    out.close()
+def enqueue_output(buffer, queue):
+    for buffer_string in iter(buffer.readline, b''):
+    # for buffer_string in iter(lambda: buffer.read(128), b''):  # just in case we want to read without newlines
+        queue.put(buffer_string)
+    buffer.close()
+
+
+class BufferMonitor(object):
+    def __init__(self, buffer):
+        self.queue = Queue()
+        self.thread = Thread(target=enqueue_output, args=(buffer, self.queue))
+        self.thread.daemon = True  # thread dies with the program
+        self.thread.start()
 
 
 class ExecutionTimeLimitExceeded(Exception):
@@ -40,68 +50,42 @@ def run(submission_id):
         cache.set("submission-%s-stderr" % submission_id, "Could not find a task with this ID (%s)" % submission_id)
         return
 
-    # for i in range(100):
-    #     # Check if cache is empty, if so we can put something there!
-    #     current_stdout = cache.get("submission-%s-stdout" % submission_id)
-    #     if current_stdout is None:
-    #         cache.set("submission-%s-stdout" % submission_id, "weeeeeeeeeeeeeeeee!!!\n")
-    #     time.sleep(0.1)
-
-
     signal.signal(signal.SIGALRM, alarm_handler)
     signal.alarm(60 * 10)  # require an "alarm" return signal within 10 min or force close
-
-
 
     stdout_buffer = ""
     stderr_buffer = ""
 
-    print "here at least"
-    # cmd = subprocess.Popen(["ping", "google.com"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # for stdout, stderr in zip(cmd.stdout, cmd.stderr):
-    #     print "Stdout:", stdout
-    #
-    #     stdout_buffer += stdout
-    #     stderr_buffer += stderr
-    #
-    #     # Check if cache is empty, if so we can put something there!
-    #     cached_stdout = cache.get("submission-%s-stdout" % submission_id)
-    #     if cached_stdout is None:
-    #         cache.set("submission-%s-stdout" % submission_id, stdout)
-    #         cache.set("submission-%s-stderr" % submission_id, stderr)
-    #         stdout_buffer = stderr_buffer = None
-    #
-    #     time.sleep(0.1)
+    process_args = ["python", "writes_to_stderr.py"]
+    # process_args = ["ping", "google.com"]
 
-    process = subprocess.Popen(["ping", "google.com"], stdout=subprocess.PIPE, bufsize=1, close_fds=ON_POSIX)
-    q = Queue()
-    t = Thread(target=enqueue_output, args=(process.stdout, q))
-    t.daemon = True  # thread dies with the program
-    t.start()
+    # In python we don't automatically flush the buffers, so if we se a python arg put a -u after it
+    for index, arg in enumerate(process_args):
+        if arg == "python":
+            process_args.insert(index - 1, "-u")  # insert the autoflush flag
+
+    process = subprocess.Popen(
+        process_args,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        bufsize=0,
+    )
+    stdout_monitor = BufferMonitor(process.stdout)
+    stderr_monitor = BufferMonitor(process.stderr)
     exit_code = None
-
-
-    #cmd = subprocess.Popen(["ping", "google.com"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #for stdout, stderr in zip(cmd.stdout, cmd.stderr):
-
-    # print "Stdout:", stdout
-    #
-    #
-    # # Check if cache is empty, if so we can put something there!
-    # cached_stdout = cache.get("submission-%s-stdout" % submission_id)
-    # if cached_stdout is None:
-    #     cache.set("submission-%s-stdout" % submission_id, stdout)
-    #     cache.set("submission-%s-stderr" % submission_id, stderr)
-    #     stdout_buffer = stderr_buffer = None
 
     try:
         while exit_code is None:
             try:
-                stdout = q.get_nowait() # or q.get(timeout=.1)
+                # stdout = stdout_monitor.queue.get_nowait()
+                stdout = stdout_monitor.queue.get(timeout=.1)
+                # stderr = stderr_monitor.queue.get_nowait()
+                stderr = stderr_monitor.queue.get(timeout=.1)
             except Empty:
                 pass
             else:
-                stdout_buffer += stdout
+                stdout_buffer += stdout if stdout else ''
+                stderr_buffer += stderr if stderr else ''
 
                 # Check if cache is empty, if so we can put something there!
                 cached_stdout = cache.get("submission-%s-stdout" % submission_id)
@@ -109,8 +93,6 @@ def run(submission_id):
                     cache.set("submission-%s-stdout" % submission_id, stdout_buffer)
                     cache.set("submission-%s-stderr" % submission_id, stderr_buffer)
                     stdout_buffer = stderr_buffer = ""
-            #stderr_buffer += stderr
-            time.sleep(0.1)
             exit_code = process.poll()
     except (ValueError, OSError):
         pass  # tried to communicate with dead process
@@ -121,30 +103,4 @@ def run(submission_id):
         # Reset the alarm, 0 means no "alarm" signal is required to respond
         signal.alarm(0)
 
-    # get submission dataset
-    # does it exist in some cache?
-    # fetch if not
-    # get script
-    # replace $INPUT with proper folder
-    # run that WITH DEMOTE! https://github.com/codalab/codalab/blob/master/codalab/codalabtools/compute/worker.py#L345
-    # pipe stdout and stderr
-
-
-
-
-    # changes we'll have to make to system
-    # create some low user
-
-
-
-
-
-
-
-
-
-
     cache.set("submission-%s-stdout" % submission_id, "-%-%-%-%-END BRAIN SEQUENCE-%-%-%-%-")
-
-
-    #call(config["run"].split())
