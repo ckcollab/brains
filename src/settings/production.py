613e8b67f5dc835fdf24e79e3b230b6b0ad7ebaf
Eric Carmichael <eric@ckcollab.com>
2015-12-19 02:19:45 -0800

    use cloudamqp since we might have fewer, larger messages instead of many small where bigwig would be better deal

--- src/settings/production.py
+++ src/settings/production.py
@@ -14,8 +14,8 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "afaefawe23af")
 assert SECRET_KEY, "Set your DJANGO_SECRET_KEY env var"
 
 # Celery
-# BROKER_URL = os.environ.get('CLOUDAMQP_URL', None)
-BROKER_URL = os.environ.get("RABBITMQ_BIGWIG_URL", None)
+BROKER_URL = os.environ.get('CLOUDAMQP_URL', None)
+# BROKER_URL = os.environ.get("RABBITMQ_BIGWIG_URL", None)
 
 assert BROKER_URL, "Celery BROKER_URL env var missing!"
 
