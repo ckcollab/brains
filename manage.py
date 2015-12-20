87c58d511b366a2985b4092847ccdf2246d04202
Eric Carmichael <eric@ckcollab.com>
2015-12-20 11:20:20 -0800

    ugh change defaults to prod

--- manage.py
+++ manage.py
@@ -4,8 +4,7 @@ import sys
 
 if __name__ == "__main__":
     sys.path.append('src')
-    settings_module = os.environ.get('DJANGO_SETTINGS_MODULE')
-    assert settings_module, "DJANGO_SETTINGS_MODULE env var is required"
+    settings_module = os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.production')
 
     from django.core.management import execute_from_command_line
 
--- src/wsgi.py
+++ src/wsgi.py
@@ -3,6 +3,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-settings_module = os.environ.setdefault('DJANGO_SETTINGS_MODULE', "settings.base")
+settings_module = os.environ.setdefault('DJANGO_SETTINGS_MODULE', "settings.production")
 
 application = Cling(get_wsgi_application())
