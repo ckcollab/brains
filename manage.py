5ffe868fea7421d7b8a2f9a85a360bcb2dfd90f1
Eric Carmichael <eric@ckcollab.com>
2015-12-20 11:14:34 -0800

    database isn't being saved...

--- manage.py
+++ manage.py
@@ -4,7 +4,8 @@ import sys
 
 if __name__ == "__main__":
     sys.path.append('src')
-    settings_module = os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
+    settings_module = os.environ.setdefault('DJANGO_SETTINGS_MODULE')
+    assert settings_module, "DJANGO_SETTINGS_MODULE env var is required"
 
     from django.core.management import execute_from_command_line
 
