6bfe0c469c211633d68618876f80257199d0e782
Eric Carmichael <eric@ckcollab.com>
2015-12-20 11:16:37 -0800

    fix env call

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     sys.path.append('src')
-    settings_module = os.environ.setdefault('DJANGO_SETTINGS_MODULE')
+    settings_module = os.environ.get('DJANGO_SETTINGS_MODULE')
     assert settings_module, "DJANGO_SETTINGS_MODULE env var is required"
 
     from django.core.management import execute_from_command_line
